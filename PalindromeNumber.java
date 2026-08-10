c6497669e6d6eec6c0cf967306d7e1d29af1d431
Abhishek Kumar <abhishekkumarsingh845454@gmail.com>
2026-08-10 21:04:01 +0530

    first commit

---
+++ PalindromeNumber.java
@@ -0,0 +1,19 @@
+public class PalindromeNumber {
+
+    public static boolean isPalindrome(int num) {
+        int original = num;
+        int reverse = 0;
+
+        while (num != 0) {
+            int digit = num % 10;
+            reverse = reverse * 10 + digit;
+            num /= 10;
+        }
+
+        return original == reverse;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(121));
+    }
+}
